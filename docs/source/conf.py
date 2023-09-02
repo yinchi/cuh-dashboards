b08055477f526e29a348bc460ed343d24ff37432
Yin-Chi Chan <ycc39@cam.ac.uk>
2023-09-02 04:15:58 +0100

    Set up sphinx documentation

---
+++ docs/source/conf.py
@@ -0,0 +1,67 @@
+"""Configuration file for the Sphinx documentation builder.
+
+For the full list of built-in configuration values, see the documentation:
+https://www.sphinx-doc.org/en/master/usage/configuration.html
+"""
+
+# pylint: disable=C0103,W0622
+
+import sys
+sys.path.insert(0, '../../hpath/')
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'CUH Histopathology Simulation Server'
+copyright = '2023, Yin-Chi Chan'
+author = 'Yin-Chi Chan'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autodoc',
+              'sphinx.ext.githubpages',
+              'sphinx.ext.intersphinx',
+              'sphinx.ext.todo',
+              'sphinx.ext.mathjax',
+              'sphinx.ext.viewcode',
+              'sphinx.ext.napoleon',]
+
+# autodoc_typehints_format = 'fully-qualified'
+autoapi_add_toctree_entry = False
+autoapi_member_order = 'bysource'
+
+autodoc_default_options = {
+    'member-order': 'groupwise',
+    'exclude-members': 'model_config, model_fields'
+}
+
+
+# Napoleon settings
+napoleon_google_docstring = True
+napoleon_numpy_docstring = True
+napoleon_include_init_with_doc = True
+# napoleon_include_private_with_doc = True
+napoleon_include_special_with_doc = True
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+intersphinx_mapping = {
+    'python': ('https://docs.python.org/3', None),
+    'pandas': ('http://pandas.pydata.org/docs/', None),
+    'numpy': ('https://numpy.org/doc/stable/', None),
+    'openpyxl': ('https://openpyxl.readthedocs.io/en/stable/', None),
+    'salabim': ('https://www.salabim.org/manual/', None),
+    'sphinx': ("https://www.sphinx-doc.org/en/master/", None),
+    'pydantic': ("https://docs.pydantic.dev/latest/", None)
+}
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+html_theme_options = {
+    'navigation_depth': 99,
+}
---
+++ docs/source/index.rst
@@ -0,0 +1,25 @@
+Addenbrooke's Hospital Histopathology Department Simulation Server
+==================================================================
+
+This documentation website describes the simulation server for modelling
+the histopathology department at Addenbrooke's Hospital, part of the
+Cambridge University Hospitals NHS Foundation Trust.
+
+**Author**: Yin-Chi Chan, Institute for Manufacturing, Univ. of Cambridge
+
+.. toctree::
+   :maxdepth: 1
+   :caption: Documentation
+
+.. toctree::
+   :maxdepth: 4
+   :caption: Python Documentation
+
+   apidoc/hpath
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
