fede2faf21e4385e6e69901ffc3ab6112262661c
Yin-Chi Chan <ycc39@cam.ac.uk>
2023-09-18 03:25:59 +0100

    Docker setup for frontend

--- frontend-lakee.Dockerfile
+++ frontend-lakee.Dockerfile
@@ -6,6 +6,10 @@ RUN tar -xzf publish.tar.gz
 WORKDIR /digital-twin-publish
 RUN npm install -g pnpm
 RUN pnpm install
+
+# Monkey-patching some files
 COPY site.config.ts web/site.config.ts
+COPY menu.tsx web/src/components/layout/menu.tsx
+
 RUN pnpm build:web
 CMD pnpm start:web
--- site.config.ts
+++ site.config.ts
@@ -4,11 +4,10 @@ as we are Dockerising our application */
 import config from "../ecosystem.config";
 const prodEnv = config.apps[0].env;
 const devEnv = config.apps[0].devEnv;
-
 const isProd = process.env.NODE_ENV === "production";
-
 const env = isProd ? prodEnv : devEnv;
-const HOST_NAME = "host.docker.internal"; // TODO: Change this for public server
+
+const HOST_NAME = "host.docker.internal"; // Change this for public server
 
 export const siteConfig = {
   DH_API_URL: `http://${HOST_NAME}:${env.DH_PORT}`,
